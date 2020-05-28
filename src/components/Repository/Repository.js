e8b6ae8abbbe50f0db6edf9165f893443ae306ab
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-28 06:16:38 -0400

    fixed a issue with SkeletonRepository

--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -198,48 +198,44 @@ const Repository = (repositoryName) => {
 };
 
 const SkeletonRepository = (id) => (
-  <Container key={id}>
-    <div>
-      <span aria-label="title">
-        <CircularProgress />
-      </span>
-      <a
-        href="#"
-        target="_blank"
-        aria-hidden="true"
-        aria-label={`github stars`}
-        title="star"
-        rel="noopener noreferrer"
-      >
-        <span role="img" aria-label="star">
-          <FontAwesomeIcon icon={["fas", "star"]} />
-          <>
-            {" "}
-            <CircularProgress />
-          </>
-        </span>
-      </a>
-      <a
-        href="#"
-        target="_blank"
-        aria-hidden="true"
-        aria-label={`fork on github`}
-        title="fork"
-        rel="noopener noreferrer"
-      >
-        <span role="img" aria-label="branch">
-          <FontAwesomeIcon icon={["fas", "code-branch"]} />
-          <>
-            {" "}
-            <CircularProgress />
-          </>
-        </span>
-      </a>
-    </div>
-    <div>
-      <SkeletonText heading={false} lineCount={2} paragraph width="100%" />
-    </div>
-  </Container>
+	<Container key={id}>
+		<div>
+			<span aria-label="title">
+				<CircularProgress />
+			</span>
+			<a
+				href="#"
+				target="_blank"
+				aria-hidden="true"
+				aria-label="github stars"
+				title="star"
+				rel="noopener noreferrer"
+			>
+				<span role="img" aria-label="star">
+					<FontAwesomeIcon icon={["fas", "star"]} />
+					{" "}
+					<CircularProgress />
+				</span>
+			</a>
+			<a
+				href="#"
+				target="_blank"
+				aria-hidden="true"
+				aria-label="fork on github"
+				title="fork"
+				rel="noopener noreferrer"
+			>
+				<span role="img" aria-label="branch">
+					<FontAwesomeIcon icon={["fas", "code-branch"]} />
+					{" "}
+					<CircularProgress />
+				</span>
+			</a>
+		</div>
+		<div>
+			<SkeletonText heading={false} lineCount={2} paragraph width="100%" />
+		</div>
+	</Container>
 );
 
 export default Repository;
